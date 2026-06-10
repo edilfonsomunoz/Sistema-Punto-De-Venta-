ff7d7aa7e299d8b967742b1d2829efea56d70ea9
edilfonsomunoz <edilfonsomunoz@users.noreply.github.com>
2026-06-09 23:31:32 -0500

    Fix Nginx routing on Railway by adding Nixpacks fallback path and removing index_page from config

--- application/config/config.php
+++ application/config/config.php
@@ -41,7 +41,7 @@ $config['base_url'] = $protocol . $host . $script_path . '/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
